b3be7bc1b172e6a8b73f10afc6c4b8520131af2d
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-06-18 12:18:07 -0700

    Convert most of file to TypeScript
    The javascript version used closures to share state between
    event handlers.  This made review of the code more difficult,
    even though it's perfectly legal to do.
    
    Converted to using objects that store state, for better
    debuggability.

--- lib/append-save-as-dialog.ts
+++ lib/append-save-as-dialog.ts
@@ -1,124 +1,206 @@
 
-function appendSaveAsDialog (index, output) {
-    var div;
-    var menu;
+import { assert_for_review } from "./assert";
 
-    function closeMenu(e) {
-        var left = parseInt(menu.style.left, 10);
-        var top = parseInt(menu.style.top, 10);
-        if (
-            e.x < left ||
-            e.x > (left + menu.offsetWidth) ||
-            e.y < top ||
-            e.y > (top + menu.offsetHeight)
-        ) {
-            menu.parentNode.removeChild(menu);
-            document.body.removeEventListener('mousedown', closeMenu, false);
-        }
+// BUGBUG -- how to get equivalent results in ESLint for Typescript?
+// Something similar to: import * from "lib.dom.d.ts" ????
+/* eslint-env browser */
+
+
+
+// there can be only a single "Save As" dialog showing at one time.
+let state : SaveAsDialog | null = null;
+
+class SaveAsDialogEventHandler implements EventListenerObject{
+
+    private constructor(readonly index: number, readonly output: string) {
+    }
+    // TODO: Determine if this leaks memory, if generating new WaveDrom images... 
+    //       It might, if those images already take a reference to the event handler object
+    //       Keeping these references here, because concerned about garbage collection
+    //       free'ing the object, while the event handler is registered.
+    private static handlers : SaveAsDialogEventHandler[] = [];
+
+    public static Attach(index: number, output:string) {
+        const elementId : string = output + index;
+        const div = document.getElementById(elementId);
+        assert_for_review(div !== null, "Unable to find element " + elementId);
+
+        const tmp = new SaveAsDialogEventHandler(index, output);
+        this.handlers.push(tmp);
+        div.childNodes[0].addEventListener('contextmenu', tmp, false);
     }
 
-    div = document.getElementById(output + index);
+    handleEvent(evt: Event): void {
+        throw new Error("Method not implemented.");
+        const mouseEvt : MouseEvent = evt as MouseEvent; // cannot directly define the parameter as this type?!
+        if ((mouseEvt !== null)) { // a mouse event ... this is the only type handled
+            if (state !== null) {
+                // explicitly remove the old one (does this occur when right-click "off" the current menu?)
+                state.cleanup();
+                state = null; // clear old state?  should we explicitly call closeMenu?
+            }
+            assert_for_review(state === null);
+            state = new SaveAsDialog(this.index, this.output, mouseEvt.x, mouseEvt.y);
+            mouseEvt.preventDefault();
+        }
+    }
+}
+class SaveAsDialog {
+    // BUGBUG -- what is the magic number 166?
+    static MAGIC_NUMBER_166 = 166;
 
-    div.childNodes[0].addEventListener('contextmenu',
-        function (e) {
-            var list, savePng, saveSvg;
+    readonly index : number;
+    readonly output : string;
+    readonly elementId : string;
+    readonly presumedDiv : HTMLElement; // This was not guaranteed to be HTMLDivElement by prior code
+    readonly menu : HTMLDivElement;
+    readonly parentNode : ParentNode;
+    readonly left : number;
+    readonly top : number;
 
-            menu = document.createElement('div');
+    public constructor(index : number, output : string, x : number, y : number) {
+        const elementId = output + index;
+        const presumedDiv = document.getElementById(elementId);
+        assert_for_review(presumedDiv !== null);
+    
+        const menu = document.createElement('div');
+        menu.className = 'wavedromMenu';
+        menu.style.top = '' + y + 'px';
+        menu.style.left = '' + x + 'px';
 
-            menu.className = 'wavedromMenu';
-            menu.style.top = e.y + 'px';
-            menu.style.left = e.x + 'px';
+        const list = document.createElement('ul');
+        const savePng = document.createElement('li');
+        savePng.innerHTML = 'Save as PNG';
+        list.appendChild(savePng);
 
-            list = document.createElement('ul');
-            savePng = document.createElement('li');
-            savePng.innerHTML = 'Save as PNG';
-            list.appendChild(savePng);
+        const saveSvg = document.createElement('li');
+        saveSvg.innerHTML = 'Save as SVG';
+        list.appendChild(saveSvg);
+        menu.appendChild(list);
 
-            saveSvg = document.createElement('li');
-            saveSvg.innerHTML = 'Save as SVG';
-            list.appendChild(saveSvg);
+        document.body.appendChild(menu); // this gives the parent node...
+        const parentNode = menu.parentNode;
+        assert_for_review(parentNode !== null);
 
-            //var saveJson = document.createElement('li');
-            //saveJson.innerHTML = 'Save as JSON';
-            //list.appendChild(saveJson);
+        // store all the state...
+        this.index = index;
+        this.output = output;
+        this.elementId = elementId;
+        this.presumedDiv = presumedDiv;
+        this.menu = menu;
+        this.parentNode = parentNode;
+        this.left = x;
+        this.top = y;
 
-            menu.appendChild(list);
+        // add menu-specific listeners
+        savePng.addEventListener('click', clickSaveAsPng, false);
+        saveSvg.addEventListener('click', clickSaveAsSvg, false);        
+        menu.addEventListener('contextmenu', justPreventDefault, false);
+        // add document-wide listener (to catch all mousedown events)
+        document.body.addEventListener('mousedown', closeMenu, false);
+    }
 
-            document.body.appendChild(menu);
+    public cleanup() : void {
+        this.parentNode.removeChild(this.menu);
+        document.body.removeEventListener('mousedown', closeMenu, false);
+    }
 
-            savePng.addEventListener('click',
-                function () {
-                    var html, firstDiv, svgdata, img, canvas, context, pngdata, a;
+    public isOutsideBounds(x: number, y: number): boolean {
+        return (
+            (x < this.left) ||
+            (y < this.top)  ||
+            (x > this.left + this.menu.offsetWidth) ||
+            (y > this.top + this.menu.offsetHeight)
+        );
+    }
 
-                    html = '';
-                    if (index !== 0) {
-                        firstDiv = document.getElementById(output + 0);
-                        html += firstDiv.innerHTML.substring(166, firstDiv.innerHTML.indexOf('<g id="waves_0">'));
-                    }
-                    html = [div.innerHTML.slice(0, 166), html, div.innerHTML.slice(166)].join('');
-                    svgdata = 'data:image/svg+xml;base64,' + btoa(html);
-                    img = new Image();
-                    img.src = svgdata;
-                    canvas = document.createElement('canvas');
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    context = canvas.getContext('2d');
-                    context.drawImage(img, 0, 0);
+    public getBase64EncodedSvg() : string {
+        let html = '';
+        if (this.index !== 0) {
+            // WaveDrom includes some information only in the first instance
+            // Second and later instances refer to that data in the first instance
+            // This ensures all data from MAGIC_NUMBER_166 through the start
+            // of '<g id="waves_0">' is included in the SVG data, which ensures
+            // the SVG data is fully self-contained.
+            const firstDiv = document.getElementById(this.output + 0);
+            assert_for_review(firstDiv !== null);
+            html += firstDiv.innerHTML.substring(SaveAsDialog.MAGIC_NUMBER_166, firstDiv.innerHTML.indexOf('<g id="waves_0">'));
+        }
+        assert_for_review(this.presumedDiv !== null);
+        html = [
+            this.presumedDiv.innerHTML.slice(0, SaveAsDialog.MAGIC_NUMBER_166),
+            html, // when not index 0, this includes items from first WaveDrom segment
+            this.presumedDiv.innerHTML.slice(SaveAsDialog.MAGIC_NUMBER_166)]
+            .join('');
+    
+        return 'data:image/svg+xml;base64,' + btoa(html);
+    }
 
-                    pngdata = canvas.toDataURL('image/png');
+    static GlobalState : SaveAsDialog | null = null;
 
-                    a = document.createElement('a');
-                    a.href = pngdata;
-                    a.download = 'wavedrom.png';
-                    a.click();
+}
 
-                    menu.parentNode.removeChild(menu);
-                    document.body.removeEventListener('mousedown', closeMenu, false);
-                },
-                false
-            );
+function justPreventDefault(evt: MouseEvent) : void {
+    evt.preventDefault();
+}
+// function closeMenu(_evt: MouseEvent) : void {
+function closeMenu() : void {
+    // when the mouse event is NOT within the bounds of the menu, then remove the menu
+    // and remove this event handler from the global document.
+    assert_for_review(state !== null);
+    if (state.isOutsideBounds(evt.x, evt.y)) {
+        state.cleanup();
+        state = null;
+        // TODO: Verify it was intentional to **NOT** call _evt.preventDefault()?
+    }
+}
+// function clickSaveAsPng(_evt: MouseEvent) : void {
+function clickSaveAsPng() : void {
+    assert_for_review(state !== null);
 
-            saveSvg.addEventListener('click',
-                function () {
-                    var html,
-                        firstDiv,
-                        svgdata,
-                        a;
+    const svgdata = state.getBase64EncodedSvg();
 
-                    html = '';
-                    if (index !== 0) {
-                        firstDiv = document.getElementById(output + 0);
-                        html += firstDiv.innerHTML.substring(166, firstDiv.innerHTML.indexOf('<g id="waves_0">'));
-                    }
-                    html = [div.innerHTML.slice(0, 166), html, div.innerHTML.slice(166)].join('');
-                    svgdata = 'data:image/svg+xml;base64,' + btoa(html);
+    // BEGIN variable part between PNG/SVG
+    const img = new Image();
+    img.src = svgdata;
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const context = canvas.getContext('2d');
+    assert_for_review(context !== null);
+    context.drawImage(img, 0, 0);
 
-                    a = document.createElement('a');
-                    a.href = svgdata;
-                    a.download = 'wavedrom.svg';
-                    a.click();
+    const pngdata = canvas.toDataURL('image/png');
 
-                    menu.parentNode.removeChild(menu);
-                    document.body.removeEventListener('mousedown', closeMenu, false);
-                },
-                false
-            );
+    const a = document.createElement('a');
+    a.href = pngdata;
+    a.download = 'wavedrom.png';
+    // END variable part between PNG/SVG
 
-            menu.addEventListener('contextmenu',
-                function (ee) {
-                    ee.preventDefault();
-                },
-                false
-            );
+    a.click();
+    state.cleanup();
+    state = null;
+}
+// function clickSaveAsSvg(_evt: MouseEvent) : void {
+function clickSaveAsSvg() : void { 
+    assert_for_review(state !== null);
 
-            document.body.addEventListener('mousedown', closeMenu, false);
+    const svgdata = state.getBase64EncodedSvg();
 
-            e.preventDefault();
-        },
-        false
-    );
+    // BEGIN variable part between PNG/SVG
+    const a = document.createElement('a');
+    a.href = svgdata;
+    a.download = 'wavedrom.svg';
+    // END variable part between PNG/SVG
+
+    a.click();
+    state.cleanup();
+    state = null;
+}
+
+function appendSaveAsDialog (index: number, output: string) {
+    SaveAsDialogEventHandler.Attach(index, output);
 }
 
 module.exports = appendSaveAsDialog;
 
-/* eslint-env browser */
