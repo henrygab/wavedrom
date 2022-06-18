2118d052fa5f4879e4028ad970e95c5aefb99c54
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-06-18 12:05:55 -0700

    add second assert, to allow differentiating
    `assert()` should be used when it's absolutely unexpected,
    such as when the function is called without

--- lib/assert.ts
+++ lib/assert.ts
@@ -9,3 +9,16 @@ export function assert(
         throw ( "ASSERT FAILED: " + msg );
     }
 }
+
+export function assert_for_review(
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    condition: any,
+    /* eslint-enable  @typescript-eslint/no-explicit-any */
+    msg?: string
+    ): asserts condition {
+    if (!condition) {
+        throw ( "ASSERT FAILED: " + msg );
+    }
+}
+
+
