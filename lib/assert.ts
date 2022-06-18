070905dde1d7bd19019d22a614127f8fe86cbc6e
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-06-18 13:40:23 -0700

    add assert_unreachable()

--- lib/assert.ts
+++ lib/assert.ts
@@ -4,21 +4,32 @@ export function assert(
     condition: any,
     /* eslint-enable  @typescript-eslint/no-explicit-any */
     msg?: string
-    ): asserts condition {
+    ): asserts condition
+{
     if (!condition) {
         throw ( "ASSERT FAILED: " + msg );
     }
 }
 
+// use assert_for_review temporarily, for assertions which may not have been fully validated
 export function assert_for_review(
     /* eslint-disable @typescript-eslint/no-explicit-any */
     condition: any,
     /* eslint-enable  @typescript-eslint/no-explicit-any */
     msg?: string
-    ): asserts condition {
+    ): asserts condition
+{
     if (!condition) {
         throw ( "ASSERT FAILED: " + msg );
     }
 }
 
 
+export function assert_unreachable(
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    _: never
+    /* eslint-enable @typescript-eslint/no-unused-vars */
+    ): never
+{
+    throw new Error("Statement should be unreachable");
+}
