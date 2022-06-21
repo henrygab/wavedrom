7f4ce2e1ffd40d833f790027a058a32e0173c168
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-06-21 13:58:14 -0700

    reduce comments

--- lib/append-save-as-dialog.ts
+++ lib/append-save-as-dialog.ts
@@ -3,62 +3,11 @@ import { assert, assert_for_review } from "./assert";
 
 /* eslint-env browser */
 
-// there can be only a single "Save As" dialog showing at one time.
-
-// `appendSaveAsDialog(index, output)` simply adds an event listener
-// for `contextmenu` (e.g., mouse right-click) events.
-
-// That `contextmenu` listener does the hard work, every time
-// there is a right-click event:
-//
-// 1. creates a popup menu (`div`) element 
-// 2. sets `div` properties: {
-//    className = 'wavedromMenu',
-//    style.top = e.y.toString() +'px',
-//    style.left = e.x.toString() + 'px',
-//    }
-// 3. creates unordered list (`ul`) with list items (`li`)
-//    for both "Save as PNG" and "Save as SVG"
-// 4. appends the unordered list to the 'div' element
-// 5. appends the popup menu 'div' element to 'document.body'
-// 6. adds event listener for 'click' to "Save as PNG" element
-// 7. adds event listener for 'click' to "Save as SVG" element
-// 8. adds event listener for 'contextMenu' to 'div' element which simply calls 'evt.preventDefault()'
-// 9. adds event listener to 'document.body' for 'mousedown', which calls 'closeMenu'
-// 10. calls 'evt.preventDefault()', so right-click isn't handled elsewhere
-
-// What the listener for "Save as PNG" element does:
-// 1. magic to get Base64-encoded SVG data
-// 2. use `Image` and `document.createElement('canvas')` to draw the SVG as an image
-// 3. use `canvas.toDataURL('image/png')` to create a URI for the image
-// 4. create an '<a href=' link element with that URI
-// 5. call `a.click()` on that URI to initiate the download of the SVG
-// 6. remove the menu 'div' element from 'document.body'
-// 7. remove the event listener 'closeMenu' for 'mousedown' event from 'document.body'
-
-// What the listener for "Save as SVG" element does:
-// 1. magic to get Base64-encoded SVG data
-// 2. create an '<a href=' link element with Base64-encoded SVG data
-// 3. ... nothing ... just for parity with save as png ...
-// 4. ... nothing ... just for parity with save as png ...
-// 5. call `a.click()` on that URI to initiate the download of the SVG
-// 6. remove the menu 'div' element from 'document.body'
-// 7. remove the event listener 'closeMenu' for 'mousedown' event from 'document.body'
-
-// What closeMenu(evt) does:
-// essentially, it cleans up when a click/touch is NOT on the newly-generated pop-up menu
-// 1. Presumes evt has properties { x : number, y : number }
-// 2. Gets the menu 'div' element's left/top (as number)
-// 3. Gets the menu 'div' element's offsetWidth and offsetHeight (as number)
-// 3. If evt.x not within menu 'div', return without doing anything
-// 4. if evt.y not within menu 'div', return without doing anything
-// 5. ... nothing ... just for parity with save as png / save as svg ...
-// 6. remove the menu 'div' element from 'document.body'
-// 7. remove the event listener 'closeMenu' for 'mousedown' event from 'document.body'
-
 // BUGBUG -- what is the magic number 166?
 const MAGIC_NUMBER_166 = 166;
 
+// `appendSaveAsDialog(index, output)` simply adds an event listener
+// that adds a popup menu, and clears when clicked on (or click outside of)
 export function appendSaveAsDialog(index: number, output: string) {
 
     // menu will be created and destroyed dynamically
