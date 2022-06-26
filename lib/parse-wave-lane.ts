2ab77d887f8cc1ab3f3427ebc4b9cf3eab6311c9
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-06-25 19:22:39 -0700

    another step forward

--- lib/parse-wave-lane.ts
+++ lib/parse-wave-lane.ts
@@ -4,26 +4,33 @@ import { genFirstWaveBrick } from './gen-first-wave-brick';
 import { genWaveBrick      } from './gen-wave-brick';
 import { findLaneMarkers   } from './find-lane-markers';
 
+// R is array of half brick types, each is item is string
+// num_unseen_markers is how many markers are now unseen due to phase
+export type parseWaveLaneResult = [ string[], number ];
+
 // src is the wave member of the signal object
 // extra = hscale-1 ( padding )
 // lane is an object containing all properties for this waveform
-function parseWaveLane (src, extra, lane) {
-    var Repeats, Top, Next, Stack = [], R = [], i, subCycle;
-    var unseen_bricks = [], num_unseen_markers;
+export function parseWaveLane (src : string, extra : number, lane : any) : parseWaveLaneResult {
+    const Stack = src.split(''); // one character at a time
+    assert_for_review(Stack.length > 0, "Else, would call genFirstWaveBrick(undefined, ...)");
+    let Next = Stack.shift();    // Next represents the next character to create symbols for
+    assert(Next !== undefined, "Else, Stack.length would have been 0");
+    let Repeats = 1;             // how many times will this symbol 
+    let subCycle = false;
 
-    Stack = src.split('');
-    Next  = Stack.shift();
-    subCycle = false;
+    var i;
 
-    Repeats = 1;
     while (Stack[0] === '.' || Stack[0] === '|') { // repeaters parser
         Stack.shift();
         Repeats += 1;
     }
+    let R : string[] = [];
     R = R.concat(genFirstWaveBrick(Next, extra, Repeats));
 
     while (Stack.length) {
-        Top = Next;
+        const Top = Next;
+        assert_for_review(Top !== undefined, "Top cannot be null, else Stack.length would have been zero");
         Next = Stack.shift();
         if (Next === '<') { // sub-cycles on
             subCycle = true;
@@ -39,15 +46,21 @@ function parseWaveLane (src, extra, lane) {
             Repeats += 1;
         }
         if (subCycle) {
-            R = R.concat(genWaveBrick((Top + Next), 0, Repeats - lane.period));
+            R = R.concat(genWaveBrick((Top + Next), 0,     Repeats - lane.period));
         } else {
-            R = R.concat(genWaveBrick((Top + Next), extra, Repeats));
+            R = R.concat(genWaveBrick((Top + Next), extra, Repeats              ));
         }
     }
+
+    const unseen_bricks : string[] = []
+    let   num_unseen_markers = 0;
     // shift out unseen bricks due to phase shift, and save them in
-    //  unseen_bricks array
+    // unseen_bricks array
     for (i = 0; i < lane.phase; i += 1) {
-        unseen_bricks.push(R.shift());
+        const potentiallyUnseen = R.shift();
+        if (potentiallyUnseen !== undefined) {
+            unseen_bricks.push(potentiallyUnseen);
+        }
     }
     if (unseen_bricks.length > 0) {
         num_unseen_markers = findLaneMarkers( unseen_bricks ).length;
